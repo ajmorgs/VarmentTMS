40e6a16600ef7c6d4b97dfddf5245fca69f10ffa
ajmorgs <ajmorgs@gmail.com>
2021-09-21 10:43:42 -0400

    had to re-add file. IntelliJ couldn't find the class

---
+++ src/main/java/com/vmtapp/enterprise/dto/Ticket.java
@@ -0,0 +1,40 @@
+package com.vmtapp.enterprise.dto;
+
+import lombok.Data;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public @Data class Ticket {
+    public String firstname;
+    public  String lastname;
+    public  String email;
+    public int id;
+    public  String status;
+    public  String assignee;
+
+    public Ticket(String firstname, String lastname, String email) throws Exception {
+        this.firstname = firstname;
+        this.lastname = lastname;
+
+        if(emailValidation(email))
+        {
+            this.email = email;
+        }
+        else
+        {
+            throw new Exception("Not a real Email Address!");
+        }
+    }
+
+    private boolean emailValidation(String email)
+    {
+        String emailRegEx = "^[A-Za-z0-9+_.-]+@(.+)$";
+
+        Pattern pattern = Pattern.compile(emailRegEx);
+
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.find();
+    }
+}
