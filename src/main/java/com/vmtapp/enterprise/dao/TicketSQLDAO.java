4699c3f941c5419fbda62ae6ee44cde6529fa506
John Stichweh <54503538+JohnStichweh@users.noreply.github.com>
2021-10-19 11:35:48 -0400

    Finished Annotations for Ticket DTO
    Annotated TicketDTO and finished TicketSQLDAO save implamentation

--- src/main/java/com/vmtapp/enterprise/dao/TicketSQLDAO.java
+++ src/main/java/com/vmtapp/enterprise/dao/TicketSQLDAO.java
@@ -1,11 +1,18 @@
 package com.vmtapp.enterprise.dao;
 
 import com.vmtapp.enterprise.dto.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository("TicketDAO")
 public class TicketSQLDAO implements ITicketDao {
+
+    @Autowired
+    TicketRepository ticketRepository;
 
     @Override
     public Ticket save(Ticket ticket) {
-        return null;
+        Ticket createdTicket = ticketRepository.save(ticket);
+        return createdTicket;
     }
 }
--- src/main/java/com/vmtapp/enterprise/dto/Ticket.java
+++ src/main/java/com/vmtapp/enterprise/dto/Ticket.java
@@ -2,13 +2,20 @@ package com.vmtapp.enterprise.dto;
 
 import lombok.Data;
 
-public @Data
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public @Data
 class Ticket {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public int id;
     public String firstName;
     public  String lastName;
     public  String email;
-    public int id;
     public  String status;
     public  String assignee;
     public String description;
